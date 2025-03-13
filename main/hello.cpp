97c9f044a24f1414f3c2c08ceb7febb8f75fd7e6
Gunda Sukesh <148301892+Gunda-Sukesh@users.noreply.github.com>
2025-03-13 16:05:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Sukesh\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
